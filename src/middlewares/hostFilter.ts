a6956bd5fe8a1be47df73292d5c7dd4f81e88dfa
Yuldoshboy Sultonov <yuldoshboy.sultonov@weblabs.pro>
2022-03-26 01:20:50 +0500

    fix hostFilter problems

--- src/middlewares/hostFilter.ts
+++ src/middlewares/hostFilter.ts
@@ -3,7 +3,9 @@ import { UnauthorizedError } from "../errors/UnauthorizedError";
 import { default as config } from "../config";
 
 export default async (req: Request, res: Response, next: NextFunction) => {
-    if (!config.allowedIPs.includes(req.ip)) {
+    const requestIp = req.headers["x-forwarded-for"] as string || req.connection.remoteAddress as string;
+
+    if (!config.allowedIPs.includes(requestIp)) {
         console.log(`IP address ${req.ip} is not allowed to scan files`);
         res.status(400).send(new UnauthorizedError("The host is not allowed"));
     } else {
